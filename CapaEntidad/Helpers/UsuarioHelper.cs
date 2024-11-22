21daf3ab3f2d0a79499f6465724fe04851f0151a
Skueletor <j.menacho@yahoo.com>
2024-11-22 18:45:35 -0400

    Agregar archivos de proyecto.

---
+++ CapaEntidad/Helpers/UsuarioHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad.Helpers
+{
+    public static class UsuarioHelper
+    {
+        public static Usuario GetAdminPredeterminado()
+        {
+            return new Usuario
+            {
+                IdUsuario = 1, //1 admin - 0 user
+                NombreCompleto = "ADMIN PREDEFINIDO",
+                Documento = "admin",
+                Clave = "admin123",
+                Estado = true,
+                oRol = new Rol { IdRol = 1, Descripcion = "Administrador" }
+            };
+        }
+
+        public static bool EsAdminPredeterminado(Usuario usuario)
+        {
+            var admin = GetAdminPredeterminado();
+            return usuario.Documento == admin.Documento && usuario.Clave == admin.Clave;
+        }
+    }
+}
